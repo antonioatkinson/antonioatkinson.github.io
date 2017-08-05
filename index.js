d2688a306e245d16f987d36c235319133d00f5a4
Antonio Thomas Atkinson <Antonio01pd2014@atkinson-antonio-thomas-2.local>
2017-08-05 12:09:56 -0400

    Added image carousel for homepage

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 $(document).ready(function(){
+
 	$(".header-button").mouseenter(function(){
 		$(this).css("color","#A9A9A9");
 	});
 	$(".header-button").mouseleave(function(){
 		$(this).css("color","white");
 	});
+	
+	$(".project-carousel").slick({
+		dots:true,
+		autoplay:true,
+		variableWidth:true
+	});
+	
+	// $('.project-carousel').slick({
+	// 	alert("slick");
+	// 	infinite: true,
+	// 	slidesToShow: 1,
+	// 	slidesToScroll: 1
+	// });
 });
