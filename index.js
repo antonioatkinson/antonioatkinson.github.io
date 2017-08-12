126f06cf6bdaefcb70cce426f6b3aff4ec250ca2
Antonio Thomas Atkinson <Antonio01pd2014@atkinson-antonio-thomas-2.local>
2017-08-12 17:31:46 -0400

    Fixed hamburger menu

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 $(document).ready(function(){
 	//Highlights button 
-	$(".header-button").mouseenter(function(){
-		$(this).addClass('hovered');
-	});
-	$(".header-button").mouseleave(function(){
-		var parent = $(this).parent();
-		var URL = $(parent).prop("href");
-		if(URL!=document.URL){
-			$(this).removeClass('hovered');
-		}
-	});
+	// $(".header-button").mouseenter(function(){
+	// 	$(this).addClass('hovered');
+	// });
+	// $(".header-button").mouseleave(function(){
+	// 	var parent = $(this).parent();
+	// 	var URL = $(parent).prop("href");
+	// 	if(URL!=document.URL){
+	// 		$(this).removeClass('hovered');
+	// 	}
+	// });
 
-	//Changes header button color based on current page 
-	$(".header-button").each(function () {
-		var parent = $(this).parent();
-		var URL = $(parent).prop("href");
-		if (URL == document.URL) {
-			$(this).addClass('hovered');
-		}
-	});
+	// //Changes header button color based on current page 
+	// $(".header-button").each(function () {
+	// 	var parent = $(this).parent();
+	// 	var URL = $(parent).prop("href");
+	// 	if (URL == document.URL) {
+	// 		$(this).addClass('hovered');
+	// 	}
+	// });
 	
-	//Carousel of photos for homepage
-	$(".project-carousel").slick({
-		dots:true,
-		autoplay:true,
-		variableWidth:true,
-		centerMode:true
-	});
+	// //Carousel of photos for homepage
+	// $(".project-carousel").slick({
+	// 	dots:true,
+	// 	autoplay:true,
+	// 	variableWidth:true,
+	// 	centerMode:true
+	// });
 });
