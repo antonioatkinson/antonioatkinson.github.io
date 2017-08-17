60498f4b8a09f947cac5a3054dad9b9d3ed10501
Antonio Thomas Atkinson <Antonio01pd2014@atkinson-antonio-thomas-2.local>
2017-08-17 00:53:48 -0400

    Readded font awesome and customized footer

--- index.js
+++ index.js
@@ -1,30 +1,63 @@
-$(document).ready(function(){
-	//Highlights button 
-	// $(".header-button").mouseenter(function(){
-	// 	$(this).addClass('hovered');
-	// });
-	// $(".header-button").mouseleave(function(){
-	// 	var parent = $(this).parent();
-	// 	var URL = $(parent).prop("href");
-	// 	if(URL!=document.URL){
-	// 		$(this).removeClass('hovered');
-	// 	}
-	// });
+// $(document).ready(function(){
+// 	//Highlights button 
+// 	// $(".header-button").mouseenter(function(){
+// 	// 	$(this).addClass('hovered');
+// 	// });
+// 	// $(".header-button").mouseleave(function(){
+// 	// 	var parent = $(this).parent();
+// 	// 	var URL = $(parent).prop("href");
+// 	// 	if(URL!=document.URL){
+// 	// 		$(this).removeClass('hovered');
+// 	// 	}
+// 	// });
 
-	// //Changes header button color based on current page 
-	// $(".header-button").each(function () {
-	// 	var parent = $(this).parent();
-	// 	var URL = $(parent).prop("href");
-	// 	if (URL == document.URL) {
-	// 		$(this).addClass('hovered');
-	// 	}
-	// });
+// 	// //Changes header button color based on current page 
+// 	// $(".header-button").each(function () {
+// 	// 	var parent = $(this).parent();
+// 	// 	var URL = $(parent).prop("href");
+// 	// 	if (URL == document.URL) {
+// 	// 		$(this).addClass('hovered');
+// 	// 	}
+// 	// });
 	
-	// //Carousel of photos for homepage
-	// $(".project-carousel").slick({
-	// 	dots:true,
-	// 	autoplay:true,
-	// 	variableWidth:true,
-	// 	centerMode:true
-	// });
+// 	// //Carousel of photos for homepage
+// 	// $(".project-carousel").slick({
+// 	// 	dots:true,
+// 	// 	autoplay:true,
+// 	// 	variableWidth:true,
+// 	// 	centerMode:true
+// 	// });
+
+//     $('[data-toggle="tooltip"]').tooltip();   
+// });
+$(document).ready(function(){
+
+	$(".fa-github").mouseenter(function(){
+		$(this).addClass("footer-hover");
+	});
+	$(".fa-github").mouseleave(function(){
+		$(this).removeClass("footer-hover");
+	});
+
+	$(".fa-envelope").mouseenter(function(){
+		$(this).addClass("footer-hover");
+	});
+	$(".fa-envelope").mouseleave(function(){
+		$(this).removeClass("footer-hover");
+	});
+
+	$(".fa-linkedin-square").mouseenter(function(){
+		$(this).addClass("footer-hover");
+	});
+	$(".fa-linkedin-square").mouseleave(function(){
+		$(this).removeClass("footer-hover");
+	});
+
+	$(".fa-codepen").mouseenter(function(){
+		$(this).addClass("footer-hover");
+	});
+	$(".fa-codepen").mouseleave(function(){
+		$(this).removeClass("footer-hover");
+	});
+      $("footer").tooltip({ selector: '[data-toggle=tooltip]' });
 });
